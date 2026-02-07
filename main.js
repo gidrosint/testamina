6cb82daad6b79a667a3cada83faa8f2c48389e6f
gidrosint <gidrosint@gmail.com>
2026-02-07 17:07:53 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("./sw.js")
+      .then(() => console.log("Service Worker зарегистрирован"))
+      .catch(err => console.error("SW ошибка:", err));
+  });
+}
